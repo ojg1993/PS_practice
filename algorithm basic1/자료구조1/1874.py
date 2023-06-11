e476c5b616a6358ff318c3074cdc1e78eb33ecae
Jingyu <ojg1993@gmail.com>
2023-06-11 11:59:24 +0900

    logic explaination modefied

--- algorithm basic1/자료구조1/1874.py
+++ algorithm basic1/자료구조1/1874.py
@@ -25,6 +25,6 @@ if temp:
 else:
     print('NO')
 
-# 1 ~ n까지 stk append(cnt가 n보다 작거나 같을때만)
+# cnt ~ n까지 stk append(n이 cnt보다 클때만, 초기값1)
 # stk[-1]이 n과 같으면 pop
 # stk[-1]이 n과 다르면 수열 생성 불가로 temp=False
