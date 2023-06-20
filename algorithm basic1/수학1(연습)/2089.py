a85e14a6f72c89bf5b7231a231ff7e554c56c745
Jingyu <ojg1993@gmail.com>
2023-06-20 22:44:31 +0900

    -bin

---
+++ algorithm basic1/수학1(연습)/2089.py
@@ -0,0 +1,16 @@
+# 2089
+
+n = int(input())
+res =''
+if n == 0:
+    print(0)
+else:    
+    while n:
+        if n % -2:
+            res = '1' + res
+            n = n//-2 + 1
+        else:
+            res = '0' + res
+            n = n//-2
+
+print(res)
