5941722ee8ff7847899caebc4b4fdf0fd6ec6a83
Jingyu <ojg1993@gmail.com>
2023-06-29 00:04:23 +0900

    이친수 top down 추가

--- algorithm basic1/다이나믹 프로그래밍 1/2193.py
+++ algorithm basic1/다이나믹 프로그래밍 1/2193.py
@@ -5,7 +5,24 @@ n = int(input())
 cache = [0] * (n+1)
 cache[1] = 1
 
+# top down
+
+def pinary_number(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif cache[n]:
+        return cache[n]
+    else:
+        cache[n] = pinary_number(n-1) + pinary_number(n-2)
+    return cache[n]
+
+print(pinary_number(n))
+
+
 # bottom up
+
 for i in range(2, n+1):
     cache[i] = cache[i-1] + cache[i-2]
 
