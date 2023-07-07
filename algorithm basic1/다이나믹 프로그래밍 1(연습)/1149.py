4f475c7a238cf92abe88627e3ab94d2465d50bba
Jingyu <ojg1993@gmail.com>
2023-07-08 00:34:18 +0900

    RGB 거리

---
+++ algorithm basic1/다이나믹 프로그래밍 1(연습)/1149.py
@@ -0,0 +1,18 @@
+# 1149
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+cache = []
+
+for i in range(n):
+    cache.append(list(map(int, input().split())))
+
+for i in range(1, n):
+    cache[i][0] = min(cache[i-1][1], cache[i-1][2]) + cache[i][0]
+    cache[i][1] = min(cache[i-1][0], cache[i-1][2]) + cache[i][1]
+    cache[i][2] = min(cache[i-1][0], cache[i-1][1]) + cache[i][2]
+
+print(min(cache[n-1][0], cache[n-1][1], cache[n-1][2]))
+
