e872c7c7e21a7d6c8da35c8a3d2d86fd5691e32a
Jingyu <ojg1993@gmail.com>
2023-08-15 21:08:18 +0900

    이전 순열

---
+++ algorithm basic2/브루트포스(순열)/10973.py
@@ -0,0 +1,16 @@
+# 10973
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+input_array = list(map(int, input().split()))
+
+for i in range(N - 1, 0, -1):
+    if input_array[i - 1] > input_array[i]:
+        for j in range(N - 1, 0, -1):
+            if input_array[i - 1] > input_array[j]:
+                input_array[i - 1], input_array[j] = input_array[j], input_array[i - 1]
+                input_array = input_array[:i] + sorted(input_array[i:],reverse=True)
+                print(*input_array)
+                exit()
+print(-1)
