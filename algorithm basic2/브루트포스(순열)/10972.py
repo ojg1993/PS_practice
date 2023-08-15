b6e6ebc45bf113b82935cc4c0bca70f67e79a731
Jingyu <ojg1993@gmail.com>
2023-08-15 21:08:08 +0900

    다음 순열

--- algorithm basic2/브루트포스(순열)/10972.py
+++ algorithm basic2/브루트포스(순열)/10972.py
@@ -1,10 +1,16 @@
 # 10972
 import sys
-from itertools import combinations
 input = sys.stdin.readline
 
 n = int(input())
-nums = sorted((map(int, input().split())))
+target = list((map(int, input().split())))
 
-for c in combinations(nums, m):
-    print(c)
+for i in range(n-1, 0, -1):
+    if target[i-1] < target[i]:
+        for j in range(n-1, 0, -1):
+            if target[i-1] < target[j]:
+                target[i-1], target[j] = target[j], target[i-1]
+                target = target[:i] + sorted(target[i:])
+                print(*target)
+                exit()
+print(-1)
