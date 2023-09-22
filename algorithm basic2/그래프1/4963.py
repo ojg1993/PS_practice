4e01f4962cf2d4ac0475d77d42b1dbec0c8c4c7f
Jingyu <ojg1993@gmail.com>
2023-09-22 18:33:23 +0900

    섬의 개수

--- algorithm basic2/그래프1/4963.py
+++ algorithm basic2/그래프1/4963.py
@@ -1,34 +1,34 @@
 # 4963
+from collections import deque
 import sys
 sys.setrecursionlimit(10**6)
 def input():
     return sys.stdin.readline().rstrip()
 
+def bfs(y, x):
+    queue = deque()
+    queue.append((y, x))
+    islands[y][x] = 0
+    while queue:
+        y, x = queue.popleft()
+        for dy, dx in d:
+            ny, nx = y + dy, x + dx
+            if (0 <= ny < h) and (0 <= nx < w) and islands[ny][nx]:
+                islands[ny][nx] = 0
+                queue.append((ny, nx))
 
-def dfs(x, y):
-    dx = [1, 1, -1, -1, 1, -1, 0, 0]
-    dy = [0, 1, 0, 1, -1, -1, 1, -1]
+if __name__ == '__main__':
+    d = [(1, 0), (-1, 0), (0, 1), (0, -1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
+    while True:
+        w, h = map(int, input().split())
+        if not w and not h:
+            break
+        islands = [list(map(int, input().split())) for _ in range(h)]
+        cnt = 0
 
-    field[x][y] = 0
-    for i in range(8):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < h and 0 <= ny < w and field[nx][ny] == 1:
-            dfs(nx, ny)
-
-
-while True:
-    w, h = map(int, input().split())
-    if not w and not h:
-        break
-    field = []
-    count = 0
-    for _ in range(h):
-        field.append(list(map(int, input().split())))
-    for i in range(h):
-        for j in range(w):
-            if field[i][j] == 1:
-                dfs(i, j)
-                count += 1
-
-    print(count)
+        for i in range(h):
+            for j in range(w):
+                if islands[i][j]:
+                    bfs(i, j)
+                    cnt += 1
+        print(cnt)
