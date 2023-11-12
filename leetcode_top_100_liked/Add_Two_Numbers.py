056d4037e82f871a4210805885d3c35b7c0145cd
Jingyu Oh <ojg1993@gmail.com>
2023-11-12 14:09:26 +0900

    Add Two Numbers

--- leetcode_top_100_liked/Add_Two_Numbers.py
+++ leetcode_top_100_liked/Add_Two_Numbers.py
@@ -6,18 +6,22 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        head  = ListNode()
-        cur = head
+        temp  = ListNode()
+        cur = temp
         carry = 0
 
-        while (l1 != None) or (l2 != None) or carry:
-            l1_val = l1.val if l1 else 0
+        while l1 or l2 or carry:
+            l1_val = l1.val if l1 else 0 # if value is None place 0 to avoid rasing erros
             l2_val = l2.val if l2 else 0
+            
+            # new digit calculation
             tot = l1_val + l2_val + carry
-            cur.next = ListNode(tot % 10)  # check !
+            cur.next = ListNode(tot % 10)
             carry = tot // 10
 
+            # pointer update
+            cur = cur.next
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
-            cur = cur.next
-        return head.next
+            
+        return temp.next
