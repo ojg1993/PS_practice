6370631cc0ebd6cc19596da36f31706efd4fb437
Jingyu <ojg1993@gmail.com>
2023-11-16 10:37:07 +0900

    Roman to Integer

---
+++ leetcode_top_100_liked/roman_to_integer.py
@@ -0,0 +1,31 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        translations = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        number = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            number += translations[char]
+        return number
+
+# class Solution:
+#     def romanToInt(self, s: str) -> int:
+#         val_dict = {"I":1,"V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+#         res = 0
+
+#         for i in range(len(s)):
+#             if i <len(s)-1 and val_dict[s[i]] < val_dict[s[i+1]]:
+#                 res -= val_dict[s[i]]
+#             else:
+#                 res += val_dict[s[i]]
+#         return res
+
