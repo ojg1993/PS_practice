b21562840f20be04fe9b0946baa42ece4776c2d5
Jingyu <ojg1993@gmail.com>
2023-11-19 21:42:55 +0900

    Letter Combinations of a Phone Number

--- leetcode_top_100_liked/letter_combinations_of_a_phone_number.py
+++ leetcode_top_100_liked/letter_combinations_of_a_phone_number.py
@@ -1,21 +1,39 @@
-class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
+# def letterCombinationsdigits(digits):
+#     if not digits:
+#         return []
 
-        if not digits:
-            return []
+#     letters = {"2":["a","b","c"], "3":["d","e","f"], "4":["g","h","i"],
+#         "5":["j","k","l"], "6":["m","n","o"], "7":["p","q","r","s"],
+#         "8":["t","u","v"], "9":["w","x","y","z"]}
+#     res = []
 
-        letters = {"2":["a","b","c"], "3":["d","e","f"], "4":["g","h","i"],
-            "5":["j","k","l"], "6":["m","n","o"], "7":["p","q","r","s"],
-            "8":["t","u","v"], "9":["w","x","y","z"]}
-        res = []
-
-        def backtrack(combination, n_digit):
-            if not n_digit:
-                res.append(combination)
-                return
+#     def backtrack(combination, n_digit):
+#         if not n_digit:
+#             res.append(combination)
+#             return
             
-            for letter in letters[n_digit[0]]:
-                backtrack(combination+letter, n_digit[1:])
+#         for letter in letters[n_digit[0]]:
+#             backtrack(combination+letter, n_digit[1:])
                 
-        backtrack("", digits)
-        return res
+#     backtrack("", digits)
+#     return res
+
+# print(letterCombinationsdigits(input()))
+
+
+from itertools import product
+
+def letterCombinationsdigits(digits):
+    if not digits:
+        return []
+
+    letters = {"2":["a","b","c"], "3":["d","e","f"], "4":["g","h","i"],
+        "5":["j","k","l"], "6":["m","n","o"], "7":["p","q","r","s"],
+        "8":["t","u","v"], "9":["w","x","y","z"]}
+    
+    array = [letters[d] for d in digits]
+    combinations = product(*array)
+    
+    return [''.join(combi) for combi in combinations]
+    
+print(letterCombinationsdigits(input()))
