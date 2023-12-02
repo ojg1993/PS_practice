ab748324a34043c78a87930753dab55c7f321e6e
Jingyu <ojg1993@gmail.com>
2023-12-02 12:28:37 +0900

    Binary Search: Search Insert Position

---
+++ leetcode_top_100_liked/search_insert_position.py
@@ -0,0 +1,17 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums)-1
+        
+        while l <= r:
+            m = (l+r) //2
+
+            if target == nums[m]:
+                return m
+
+            if target > nums[m]:
+                l = m + 1
+            else:
+                r = m - 1
+                
+        return l
