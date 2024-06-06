cff823846b85bfcf7d50fbea3560151d277479f5
Jingyu Oh <ojg1993@gmail.com>
2024-06-06 20:55:59 +0900

    26 -easy

--- leetcode_top_interview_150/array-string/remove_duplicates_from_sorted_array.py
+++ leetcode_top_interview_150/array-string/remove_duplicates_from_sorted_array.py
@@ -1,5 +1,6 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        # Two pointers
         k = 0
 
         for i in range(1, len(nums)):
@@ -7,3 +8,8 @@ class Solution:
                 k += 1
                 nums[k] = nums[i]
         return k + 1
+
+        # Sort and set in place
+        # nums[:] = sorted(set(nums))
+
+        # return len(nums)
