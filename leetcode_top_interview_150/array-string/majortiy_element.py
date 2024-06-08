3374312c7744e1994074994dd1bfe4da7464f30e
Jingyu Oh <ojg1993@gmail.com>
2024-06-08 10:51:17 +0900

    169 - easy

--- leetcode_top_interview_150/array-string/majortiy_element.py
+++ leetcode_top_interview_150/array-string/majortiy_element.py
@@ -1,8 +1,20 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        num_dict = {key: 0 for key in nums}
+        # O(1) space, O(n) time
+        cnt = 1
+        res = nums[0]
 
-        for num in nums:
-            num_dict[num] += 1
+        for cur in nums[1:]:
+            cnt += 1 if res == cur else -1
+            if cnt < 1:
+                res = cur
+                cnt = 1
+        return res
 
-        return max(num_dict, key=num_dict.get)
+        # hash: O(n) space, O(n) time
+        # num_dict = {key: 0 for key in nums}
+
+        # for num in nums:
+        #     num_dict[num] += 1
+
+        # return max(num_dict, key=num_dict.get)
