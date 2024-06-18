a9a5d16d069b1eb8cfd0954ebc997822cc0aa4ae
Jingyu <ojg1993@gmail.com>
2024-06-18 16:04:33 +0900

    238 - medium

---
+++ leetcode_top_interview_150/array-string/product_of_array_except_self.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        res = [1] * len(nums)
+        for i in range(1, len(nums)):
+            res[i] = res[i - 1] * nums[i - 1]
+
+        postfix = nums[-1]
+        for i in range(len(nums) - 2, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+
+        return res
