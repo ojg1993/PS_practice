86eb1c27ed7d6e6e0731ac6f020e645eb7117827
Jingyu <ojg1993@gmail.com>
2024-06-21 00:12:37 +0900

    LL: Find Middle Node

---
+++ data_structures_and_algorithms/linked_list/find_middle_node.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        return True
+
+    def find_middle_node(self):
+        f = self.head
+        s = self.head
+
+        while f and f.next:
+            s = s.next
+            f = f.next.next
+        return s
+
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.append(5)
+
+print(my_linked_list.find_middle_node().value)
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    3
+"""
