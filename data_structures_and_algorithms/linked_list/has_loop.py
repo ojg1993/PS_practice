71abe03436f949a888815efe0fe5b6733ccff633
Jinja <ojg1993@gmail.com>
2024-06-22 10:42:14 +0900

    LL: has loop

---
+++ data_structures_and_algorithms/linked_list/has_loop.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def has_loop(self):
+        s = self.head
+        f = self.head
+
+        while f and f.next:
+            f = f.next.next
+            s = s.next
+
+            if f == s:
+                return True
+        return False
