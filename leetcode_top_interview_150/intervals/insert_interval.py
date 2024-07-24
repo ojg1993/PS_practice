d9b8dbebf5ea34a8b0ef71784735cba76c1fd98e
Jingyu <ojg1993@gmail.com>
2024-07-24 10:47:02 +0900

    57 - medium

---
+++ leetcode_top_interview_150/intervals/insert_interval.py
@@ -0,0 +1,26 @@
+class Solution:
+    def insert(self, intervals, newInterval):
+        """
+        1. intervals are not overlapping
+        2. intervals are sorted
+        3. insert newInterval into intervals
+          case
+            a. newInterval is not overlapping and less the current interval
+            b. newInterval is not overlapping and bigger than the current interval
+            c. newInterval is overlapping the current interval
+        Int:    ---    --- --- ---
+        New: --     --   ------    ---
+        """
+
+        res = []
+        for interval in intervals:
+            if newInterval[1] < interval[0]:
+                res.append(newInterval)
+                newInterval = interval
+            elif newInterval[0] > interval[1]:
+                res.append(interval)
+            else:
+                newInterval = [min(newInterval[0], interval[0]), max(newInterval[1], interval[1])]
+
+        res.append(newInterval)
+        return res
