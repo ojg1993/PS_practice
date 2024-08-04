3aa1bc2211f6bfa4177f509a77ec6fe34ea68fb0
Jinja <ojg1993@gmail.com>
2024-08-04 12:22:39 +0900

    86 - medium

---
+++ leetcode_top_interview_150/linked-list/partition_list.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        d1 = less = ListNode(0)
+        d2 = greater = ListNode(0)
+
+        while head:
+            if head.val < x:
+                less.next = head
+                less = less.next
+            else:
+                greater.next = head
+                greater = greater.next
+            head = head.next
+
+        greater.next = None
+        less.next = d2.next
+        return d1.next
