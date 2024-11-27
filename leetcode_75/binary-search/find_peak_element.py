371866961089f22ef5564250fd6424ee78823b47
Jingyu <ojg1993@gmail.com>
2024-11-27 13:31:23 +0000

    162 - medium

---
+++ leetcode_75/binary-search/find_peak_element.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        l, r = 0, len(nums)-1
+
+        while l <= r:
+            m = (l+r) // 2
+
+            if 0 < m and nums[m] < nums[m-1]:
+                r = m - 1
+            elif m < len(nums)-1 and nums[m] < nums[m+1]:
+                l = m + 1
+            else:
+                return m
+
